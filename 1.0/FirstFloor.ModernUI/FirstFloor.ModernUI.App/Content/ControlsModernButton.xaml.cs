9ca5afba77b4524e4e8ac774cf64fc127e9c3620
kozw_cp <SND\kozw_cp>
2013-03-18 17:41:31 -0700

    added icons to the ModernButton sample page

--- 1.0/FirstFloor.ModernUI/FirstFloor.ModernUI.App/Content/ControlsModernButton.xaml.cs
+++ 1.0/FirstFloor.ModernUI/FirstFloor.ModernUI.App/Content/ControlsModernButton.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using FirstFloor.ModernUI.Windows.Controls;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Xml.Linq;
 
 namespace FirstFloor.ModernUI.App.Content
 {
@@ -23,6 +25,32 @@ namespace FirstFloor.ModernUI.App.Content
         public ControlsModernButton()
         {
             InitializeComponent();
+
+            // find all embedded XAML icon files
+            var assembly = GetType().Assembly;
+            var iconResourceNames = from name in assembly.GetManifestResourceNames()
+                                    where name.StartsWith("FirstFloor.ModernUI.App.Assets.appbar.")
+                                    select name;
+
+
+            foreach (var name in iconResourceNames) {
+                // load the resource stream
+                using (var stream = assembly.GetManifestResourceStream(name)) {
+                    // parse the icon data using xml
+                    var doc = XDocument.Load(stream);
+
+                    var path = doc.Root.Element("{http://schemas.microsoft.com/winfx/2006/xaml/presentation}Path");
+                    if (path != null) {
+                        var data = (string)path.Attribute("Data");
+
+                        // create a modern button and add it to the button panel
+                        ButtonPanel.Children.Add(new ModernButton {
+                            IconData = PathGeometry.Parse(data),
+                            Margin = new Thickness(0, 0, 4, 8)
+                        });
+                    }
+                }
+            }
         }
     }
 }
