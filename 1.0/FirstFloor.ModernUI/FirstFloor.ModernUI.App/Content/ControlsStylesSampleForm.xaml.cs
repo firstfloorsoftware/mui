5681e61bcb6f6fc4a825df75a5f1cf8e0d768d77
kozw_cp <SND\kozw_cp>
2013-03-31 12:20:58 -0700

    updated control focus styles new sample form in demo app (controls > styles > sample form)

---
+++ 1.0/FirstFloor.ModernUI/FirstFloor.ModernUI.App/Content/ControlsStylesSampleForm.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FirstFloor.ModernUI.App.Content
+{
+    /// <summary>
+    /// Interaction logic for ControlsStylesSampleForm.xaml
+    /// </summary>
+    public partial class ControlsStylesSampleForm : UserControl
+    {
+        public ControlsStylesSampleForm()
+        {
+            InitializeComponent();
+
+            this.Loaded += OnLoaded;
+        }
+
+        void OnLoaded(object sender, RoutedEventArgs e)
+        {
+            // select first control on the form
+            Keyboard.Focus(this.TextFirstName);
+        }
+    }
+}
---
+++ 1.0/FirstFloor.ModernUI/FirstFloor.ModernUI.App/SampleFormViewModel.cs
@@ -0,0 +1,60 @@
+﻿using FirstFloor.ModernUI.Presentation;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstFloor.ModernUI.App
+{
+    public class SampleFormViewModel
+        : NotifyPropertyChanged, IDataErrorInfo
+    {
+        private string firstName = "John";
+        private string lastName;
+
+        public string FirstName
+        {
+            get { return this.firstName; }
+            set
+            {
+                if (this.firstName != value) {
+                    this.firstName = value;
+                    OnPropertyChanged("FirstName");
+                }
+            }
+        }
+
+        public string LastName
+        {
+            get { return this.lastName; }
+            set
+            {
+                if (this.lastName != value) {
+                    this.lastName = value;
+                    OnPropertyChanged("LastName");
+                }
+            }
+        }
+
+        public string Error
+        {
+            get { return null; }
+        }
+
+        public string this[string columnName]
+        {
+            get
+            {
+                if (columnName == "FirstName") {
+                    return string.IsNullOrEmpty(this.firstName) ? "Required value" : null;
+                }
+                if (columnName == "LastName") {
+                    return string.IsNullOrEmpty(this.lastName) ? "Required value" : null;
+                }
+                return null;
+            }
+        }
+    }
+}
