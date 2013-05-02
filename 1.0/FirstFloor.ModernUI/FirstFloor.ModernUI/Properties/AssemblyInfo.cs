cc209375bded8d45c8ca6754d4c891903e2c76ff
kozw_cp <SND\kozw_cp>
2013-05-02 05:54:44 -0700

    added NeutralResourcesLanguageAttribute to AssemblyInfo

--- 1.0/FirstFloor.ModernUI/FirstFloor.ModernUI/Properties/AssemblyInfo.cs
+++ 1.0/FirstFloor.ModernUI/FirstFloor.ModernUI/Properties/AssemblyInfo.cs
@@ -3,6 +3,7 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using System.Windows;
 using System.Windows.Markup;
+using System.Resources;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
@@ -56,3 +57,4 @@ using System.Windows.Markup;
     //(used if a resource is not found in the page, 
     // app, or any theme specific resource dictionaries)
 )]
+[assembly: NeutralResourcesLanguageAttribute("en-US")]
