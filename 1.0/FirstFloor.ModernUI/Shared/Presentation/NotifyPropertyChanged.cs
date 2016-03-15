a5d0804ad3286fbd5e157782c7aefa49e78720fb
Koen Zwikstra <kozw@firstfloorsoftware.com>
2016-03-15 10:32:45 +0100

    added property setter helper to base NotifyPropertyChanged with .NET 4.5 CallerMemberName support

--- 1.0/FirstFloor.ModernUI/Shared/Presentation/NotifyPropertyChanged.cs
+++ 1.0/FirstFloor.ModernUI/Shared/Presentation/NotifyPropertyChanged.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -29,5 +30,25 @@ namespace FirstFloor.ModernUI.Presentation
                 handler(this, new PropertyChangedEventArgs(propertyName));
             }
         }
+
+#if !NET4
+        /// <summary>
+        /// Updates specified value, and raises the <see cref="PropertyChanged"/> event when the value has changed.
+        /// </summary>
+        /// <typeparam name="T">The type of the value.</typeparam>
+        /// <param name="storage">The current stored value</param>
+        /// <param name="value">The new value</param>
+        /// <param name="propertyName">The optional property name, automatically set to caller member name when not set.</param>
+        /// <returns>Indicates whether the value has changed.</returns>
+        protected bool Set<T>(ref T storage, T value, [CallerMemberName]string propertyName = null)
+        {
+            if (!object.Equals(storage, value)) {
+                storage = value;
+                OnPropertyChanged(propertyName);
+                return true;
+            }
+            return false;
+        }
+#endif
     }
 }
